5a2f3b330fc37f4060d05af2b9a5b808cd9d1cb7
jmann-ts <john@travelspike.com>
2016-04-07 19:32:50 -0400

    Added some scope

--- Sandbox/sandbox.js
+++ Sandbox/sandbox.js
@@ -7,4 +7,16 @@ function generateStuff() {
     results.innerHTML = words[currentIndex];
 }
 
+var x = 10;
+
+console.log(x);
+
+function add3(){
+    x = x + 3;
+};
+add3();
+console.log(x);
+add3();
+console.log(x);
+
 document.getElementById("test").addEventListener("click", generateStuff);
