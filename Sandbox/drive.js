05c84d9d0957495d95f74fcdd2ee61584b7b06a9
jmann-ts <john@travelspike.com>
2016-04-07 20:35:37 -0400

    Added Age Test

---
+++ Sandbox/drive.js
@@ -0,0 +1,22 @@
+// Do something here;
+
+function canIDrive(){
+    var ageElement = document.getElementById("age");
+    var age = ageElement.value;
+    var result = document.getElementById("results");
+    if (age < 15){
+        result.innerHTML = "You can't get a permit.";
+    }else if (age == 15){
+        result.innerHTML = "You can get a permit";
+    }else if (age < 21 && age >= 16){
+        result.innerHTML = "You can drive";
+    }else if (age < 25 && age >= 21){
+        result.innerHTML = "You can drink";
+    }
+};
+
+var btnDrive = document.getElementById('CanIDrive');
+
+btnDrive.addEventListener('click',canIDrive);
+
+
